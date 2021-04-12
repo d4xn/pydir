ed3cf5a64585cf8ae1a69600b182c9ef75d391d5
d4xn <d4nxpr0gr4mm3r@gmail.com>
2021-04-12 21:05:57 +0200

    Change on pydir.py, now the script uses args instead of inputs

--- pydir.py
+++ pydir.py
@@ -1,38 +1,41 @@
-# PYDIR 
+# PYDIR
 
 # Author > d4xn
 
-# Imports
+# ▼ Imports ▼
 
+import sys
 import requests
 
-# Printing the usage of the program
-print('''
-USAGE > python3 pydir.py
-URL (the url address) > Example -> https:// www.web.com 
-WORDLIST PATH (wordlist path) > Example -> dir1/dict.txt OR /home/user/Desktop/dict.txt 
-EXTENSION (not obligatory | without . | extension added at the end) > Example -> php OR txt \n''')
+# ▼ Inputs, args and variables ▼ 
 
-# Inputs and variables
+# The usage of the script
+help_message = '''
+🐍 PYDIR 🐍
+USAGE > python3 pydir.py -u [URL] -w [WORDLIST PATH] -e (Not Obligatory) [EXTENSION]
+EXAMPLE > python3 pydir.py -u https:// www .web .com/ -w /home/user/Desktop/wordlists/dict.txt -e php
+▼ ARGS ▼
+-h || --help > Shows the usage of the script
+-u || --url > The URL of the web we will make the requests
+-w || --wordlist > Your wordlist path
+-e || --extension > The extension that will be added at the end of the url. It isn't needed
+\n'''
 
-# URL
-url = str(input('URL > '))
+# The arguments
+args = sys.argv
 
-# Wordlist path
-wordlist = str(input('WORDLIST PATH > '))
+# The variables that we need to run the script successfully
+# Then in the main function the value of these variables will change
+url, wordlist, ext = '', '', ''
 
-# Extension added at the end
-ext = str(input('EXTENSION (without .) > '))
-
-# Functions
+# ▼ Functions ▼
 
 '''
-This function make the requests and checks the status code of them
-If the status code is equals to 200, 301 or 403
-The program prints in the console the type of request, the full url and the status code
+This function make the requests and checks the status code of them,
+if the status code is equals to 200, 
+the program prints in the console the type of request, the full url and status code
 '''
-
-def main():
+def make_request(url, wordlist, ext):
     with open(wordlist) as w:
         dirs = w.readlines()
 
@@ -46,17 +49,17 @@ def main():
                     if r_get.status_code == 200 or r_get.status_code == 301 or r_get.status_code == 403:
                         print(f'\nGET - {url}{d}.{ext} - {r_get.status_code}')
         
-                    if r_post.status_code == 200 or r_post.status_code == 301 or r_post.status_code == 403:
-                        print(f'\nPOST - {url}{dir}.{ext} - {r_post.status_code}')
+                    elif r_post.status_code == 200 or r_post.status_code == 301 or r_post.status_code == 403:
+                        print(f'\nPOST - {url}{d}.{ext} - {r_post.status_code}')
             
                 elif url[-1] != '/':
-                    r_get = requests.get(f'{url}/{dir}.{ext}')
-                    r_post = requests.post(f'{url}/{dir}.{ext}')
+                    r_get = requests.get(f'{url}/{d}.{ext}')
+                    r_post = requests.post(f'{url}/{d}.{ext}')
                     if r_get.status_code == 200 or r_get.status_code == 301 or r_get.status_code == 403:
-                        print(f'\nGET - {url}/{dir}.{ext} - {r_get.status_code}')
+                        print(f'\nGET - {url}/{d}.{ext} - {r_get.status_code}')
                 
-                    if r_post.status_code == 200 or r_post.status_code == 301 or r_post.status_code == 403:
-                        print(f'\nPOST - {url}/{dir}.{ext} - {r_post.status_code}')
+                    elif r_post.status_code == 200 or r_post.status_code == 301 or r_post.status_code == 403:
+                        print(f'\nPOST - {url}/{d}.{ext} - {r_post.status_code}')
             elif not ext:
                 if url[-1] == '/':
                     r_get = requests.get(f'{url}{d}')
@@ -65,20 +68,59 @@ def main():
                     if r_get.status_code == 200 or r_get.status_code == 301 or r_get.status_code == 403:
                         print(f'\nGET - {url}{d} - {r_get.status_code}')
         
-                    if r_post.status_code == 200 or r_post.status_code == 301 or r_post.status_code == 403:
-                        print(f'\nPOST - {url}{dir} - {r_post.status_code}')
+                    elif r_post.status_code == 200 or r_post.status_code == 301 or r_post.status_code == 403:
+                        print(f'\nPOST - {url}{d} - {r_post.status_code}')
             
                 elif url[-1] != '/':
-                    r_get = requests.get(f'{url}/{dir}')
-                    r_post = requests.post(f'{url}/{dir}')
+                    r_get = requests.get(f'{url}/{d}')
+                    r_post = requests.post(f'{url}/{d}')
                     if r_get.status_code == 200 or r_get.status_code == 301 or r_get.status_code == 403:
-                        print(f'\nGET - {url}/{dir} - {r_get.status_code}')
+                        print(f'\nGET - {url}/{d} - {r_get.status_code}')
                 
-                    if r_post.status_code == 200 or r_post.status_code == 301 or r_post.status_code == 403:
-                        print(f'\nPOST - {url}/{dir} - {r_post.status_code}')         
+                    elif r_post.status_code == 200 or r_post.status_code == 301 or r_post.status_code == 403:
+                        print(f'\nPOST - {url}/{d} - {r_post.status_code}')         
 
-# End of the function
+    # End of the function
+
+
+def main(url, wordlist, ext):
+    try:
+        if len(args) == 1:
+            print(help_message)
+    
+        elif args[1] == '-h' or args[1] == '--help':
+            print(help_message)
+    
+        elif args[1] == '-u' or args[1] == '--url':
+            url = str(args[2])
+            
+            if args[3] == '-w' or args[3] == '--wordlist':
+                wordlist = str(args[4])
+
+                if len(args) >= 7:
+                    if args[5] == '-e' or args[5] == '-extension':
+                        ext = str(args[6])
+                    
+                    else:
+                        print(f'{help_message}Sintax Error!')
+                        
+                else:
+                    pass
+                    
+            else:
+                    print(f'{help_message}Sintax Error!')
+                
+        else:
+            print(f'{help_message}Sintax Error!')
+                
+    except:
+        print(f'{help_message}Sintax Error!')
+        
+    if url != '' and wordlist != '':
+        make_request(url, wordlist, ext)
+        
+    # End of the function
 
 if __name__ == "__main__":
-    # Executing the function
-    main()
+    # Executing the function 
+    main(url, wordlist, ext)
